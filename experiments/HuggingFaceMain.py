6474ec549f55941c9a4c048915f62ca7a48ec0c8
Victor Conchello <victorconchello@gmail.com>
2025-05-02 09:13:35 +0000

    Resume runs

--- experiments/HuggingFaceMain.py
+++ experiments/HuggingFaceMain.py
@@ -2,9 +2,11 @@ from trl import GRPOTrainer
 from transformers import EarlyStoppingCallback
 
 from functools import partial, update_wrapper
+from omegaconf import OmegaConf
 from pickle import load
 import inspect
 import hydra
+import wandb
 import sys
 import os
 
@@ -19,32 +21,39 @@ from llm_utils import LLM
 @hydra.main(config_path='config', config_name='config', version_base=None)
 def __main__(config):
     
+    ### Logging ###
     os.environ["WANDB_PROJECT"] = config.wandb.project
     
     os.makedirs(f"{config.logs.folder}{config.run_name}", exist_ok=True)
-    general_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.general}", "w")
-    metics_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.metrics}", "w")
-    conversation_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.conversations}", "w")
-    eval_conversation_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.eval_conversations}", "w")
+    write_mode = "a" if config.train.resume_from_checkpoint else "w"
+    general_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.general}", write_mode)
+    metics_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.metrics}", write_mode)
+    conversation_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.conversations}", write_mode)
+    eval_conversation_file = open(f"{config.logs.folder}{config.run_name}/{config.logs.eval_conversations}", write_mode)
     sys.stdout = general_file
     sys.stderr = general_file
 
+    ### Models ###
     train_llm = LLM(config.llms.train_llm_name, lora_config=config.lora, unsloth=config.llms.unsloth).to('cuda')
     opponent_llm = LLM(config.llms.opponent_llm_name, unsloth=config.llms.unsloth).to('cuda')
 
+    ### Dataset ###
     keep_partial_conversations = config.train.method != 'star' # ugly
     with open(config.dataset.data, 'rb') as f:
         data = load(f)
     dataset = GameDataset(train_llm, opponent_llm, data, config, keep_partial_conversations=keep_partial_conversations)
 
+    ### Callbacks ###
     dataset_callback = OutdateDatasetCallback(dataset)
     metrics_logger = MetricsLogger(dataset, metics_file, conversation_file, eval_conversation_file, config)
     earlyStop = EarlyStoppingCallback(config.train.early_stopping_patience)
     callbacks = [dataset_callback, metrics_logger, earlyStop]
 
+    ### Reward function ###
     reward_mod = partial(game_reward, train_llm=train_llm, opponent_llm=opponent_llm, conversation_file=conversation_file, config=config)
     update_wrapper(reward_mod, game_reward)
     
+    ### Trainer + Config ###
     Config = {
         "grpo" : CustomGRPOConfig,
         "dpo" : CustomDPOConfig,
@@ -75,7 +84,7 @@ def __main__(config):
         eval_dataset=dataset,
         callbacks=callbacks,
     )
-    trainer.train()
+    trainer.train(resume_from_checkpoint=config.train.resume_from_checkpoint)
 
 if __name__ == "__main__":
     __main__()
